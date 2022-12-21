2b58b75a83962c1cf0d4a372b3fc89a33f2e3d9f
Artyom Kolpakov <ddvamp007@gmail.com>
2022-12-21 18:58:07 +0300

    Better formatting

--- utils/string_builder.h
+++ utils/string_builder.h
@@ -8,7 +8,7 @@
 
 namespace utils {
 
-inline ::std::string reservedString(::std::size_t capacity)
+inline ::std::string make_reserved_string(::std::size_t capacity)
 {
 	::std::string str;
 	str.reserve(capacity);
@@ -24,47 +24,47 @@ inline constexpr str_t str{};
 struct view_t {};
 inline constexpr view_t view{};
 
-class StringBuilder {
+class string_builder {
 private:
 	::std::ostringstream os_;
 
 public:
-	StringBuilder() = default;
+	string_builder() = default;
 
-	explicit StringBuilder(::std::size_t capacity)
-		: os_(reservedString(capacity))
+	explicit string_builder(::std::size_t capacity)
+		: os_(make_reserved_string(capacity))
 	{}
 	
 
-	explicit StringBuilder(::std::string init)
+	explicit string_builder(::std::string init)
 		: os_(::std::move(init))
 	{}
 
-	StringBuilder &operator<< (auto const &next)
+	string_builder &operator<< (auto const &next)
 	{
 		os_ << next;
 		return *this;
 	}
 
-	StringBuilder &operator<< (::std::ios_base &(*manip)(::std::ios_base &))
+	string_builder &operator<< (::std::ios_base &(*manip)(::std::ios_base &))
 	{
 		os_ << manip;
 		return *this;
 	}
 
-	StringBuilder &operator<< (::std::ios &(*manip)(::std::ios &))
+	string_builder &operator<< (::std::ios &(*manip)(::std::ios &))
 	{
 		os_ << manip;
 		return *this;
 	}
 
-	StringBuilder &operator<< (::std::ostream &(*manip)(::std::ostream &))
+	string_builder &operator<< (::std::ostream &(*manip)(::std::ostream &))
 	{
 		os_ << manip;
 		return *this;
 	}
 
-	StringBuilder &&operator<< (move_t) noexcept
+	string_builder &&operator<< (move_t) noexcept
 	{
 		return ::std::move(*this);
 	}
