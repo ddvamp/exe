98f0f76486d426b0f2a66c1cfda3fc24c19c2a1f
Artyom Kolpakov <ddvamp007@gmail.com>
2022-12-21 17:09:55 +0300

    Add inline executor

---
+++ exe/executors/inline.h
@@ -0,0 +1,25 @@
+#ifndef DDV_EXE_EXECUTORS_INLINE_H_
+#define DDV_EXE_EXECUTORS_INLINE_H_ 1
+
+#include "exe/executors/executor.h"
+
+namespace exe::executors {
+
+// executes task immediately at place
+class InlineExecutor : public IExecutor {
+public:
+	void execute(TaskBase *task) noexcept override
+	{
+		task->run();
+	}
+};
+
+[[nodiscard]] inline IExecutor &getInlineExecutor() noexcept
+{
+	static InlineExecutor instance;
+	return instance;
+}
+
+} // namespace exe::executors
+
+#endif /* DDV_EXE_EXECUTORS_INLINE_H_ */
