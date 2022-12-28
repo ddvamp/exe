caf9e49f8a2322cc9e6805c6ad5e47cd1ea66bef
Artyom Kolpakov <ddvamp007@gmail.com>
2022-12-28 14:51:31 +0300

    Add context::Stack

---
+++ context/stack.h
@@ -0,0 +1,64 @@
+#ifndef DDV_CONTEXT_STACK_H_
+#define DDV_CONTEXT_STACK_H_ 1
+
+#include <cstddef>
+#include <utility>
+
+#include "utils/memory/page_allocation.h"
+
+namespace context {
+
+class Stack {
+private:
+	::utils::PageAllocation allocation_;
+
+public:
+	~Stack() = default;
+
+	Stack(Stack const &) = delete;
+	void operator= (Stack const &) = delete;
+
+	Stack(Stack &&that) noexcept = default;
+	Stack &operator= (Stack &&that) noexcept = default;
+
+public:
+	Stack() noexcept = default;
+
+	static Stack allocatePages(::std::size_t count)
+	{
+		auto allocation = ::utils::PageAllocation::allocatePages(count + 1);
+		allocation.protectPages(0, 1);
+		return Stack{::std::move(allocation)};
+	}
+
+	static Stack allocateBytes(::std::size_t at_least)
+	{
+		::std::size_t const page_size = ::utils::PageAllocation::pageSize();
+
+		::std::size_t pages = at_least / page_size;
+		if (at_least % page_size != 0) {
+			++pages;
+		}
+
+		return allocatePages(pages);
+	}
+
+	::std::size_t allocationSize() const noexcept
+	{
+		return allocation_.size();
+	}
+
+	void *bottom() const noexcept
+	{
+		return allocation_.end();
+	}
+
+private:
+	Stack(::utils::PageAllocation allocation) noexcept
+		: allocation_(::std::move(allocation))
+	{}
+};
+
+} // namespace context
+
+#endif /* DDV_CONTEXT_STACK_H_ */
