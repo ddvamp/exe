9a0a1cc7f61fb411778d62dc0b4dea1312b75877
Artyom Kolpakov <ddvamp007@gmail.com>
2023-05-03 10:08:34 +0300

    Rework `fibers::WaitPoint`

--- exe/fibers/sync/wait_point.h
+++ exe/fibers/sync/wait_point.h
@@ -18,28 +18,34 @@
 namespace exe::fibers {
 
 // Synchronization primitive for waiting for the completion of tasks,
-// which are expressed as a 32-bit counter
-// Formally, the wait ends when the counter reaches zero
+// which are expressed as a 31-bit counter
+// Formally, the wait ends when the counter drops to zero
 // 
-// The add call should happens before the corresponding done call
-// (otherwise, we get a negative counter)
+// The add call should happens before the corresponding done call, otherwise
+// the behavior is undefined
 // 
 // The counter increment and wait operations are independent
 // (WaitPoint is not one-time),
 // so you can wait without even adding new tasks. However, if WaitPoint is
-// used to wait for specific tasks to complete, the add call should be
+// used to wait for specific tasks to complete, the add call should
 // happens before the wait call
 class WaitPoint {
 private:
-	inline static constexpr ::std::uint64_t helping_bit = 0x8000'0000;
+	using state_t = ::std::uint64_t;
+
+	enum Mask : state_t {
+		counter = 0x7FFF'FFFF,
+		helping_bit = 0x8000'0000,
+	};
+
+	inline static constexpr auto align =
+		::std::hardware_destructive_interference_size;
 
 	// [  32bit  |   bit   |  31bit  ]
 	// [ version | helping | counter ]
-	alignas(::std::hardware_destructive_interference_size)
-		::std::atomic_uint64_t state_ = helping_bit; // helping_bit is set
-
-	alignas(::std::hardware_destructive_interference_size)
-		Mutex m_; // protects the condvar
+	// initially, helping bit is set
+	alignas(align) ::std::atomic<state_t> state_ = Mask::helping_bit;
+	alignas(align) Mutex m_; // protects the condvar
 	CondVar counter_is_zero_{m_};
 
 public:
@@ -54,117 +60,172 @@ public:
 public:
 	WaitPoint() = default;
 
-	// increases the counter
-	void add(::std::uint64_t delta = 1) noexcept
+	// increases the counter by delta
+	void add(state_t const delta = 1) noexcept
 	{
 		// increasing logically
-		[[maybe_unused]] auto state
-			= state_.fetch_sub(delta, ::std::memory_order_relaxed);
+		[[maybe_unused]] auto const prev_state =
+			state_.fetch_sub(delta, ::std::memory_order_relaxed);
 
 		UTILS_ASSERT(
-			delta - 1 < getCounter(state - 1),
+			isAddOk(prev_state, delta),
 			"increment must be non-zero and "
 			"not overflow the 31-bit counter"
 		);
 	}
 
-	// reduces the counter, releases a data and,
-	// if the counter has reached zero, tries to notify
-	void done(::std::uint64_t delta = 1) noexcept
+	// reduces the counter by delta, (if requested) performs synchronization,
+	// and if the counter drops to zero (logically),
+	// unblocks all threads currently waiting for
+	void done(state_t const delta = 1, ::std::memory_order order =
+		::std::memory_order_seq_cst) noexcept
 	{
 		// reducing logically
-		auto state = state_.fetch_add(delta, ::std::memory_order_release);
+		auto const prev_state = state_.fetch_add(delta, order);
 
 		UTILS_ASSERT(
-			delta - 1 < getCounter(0 - state),
+			isDoneOk(prev_state, delta),
 			"decrement must be non-zero and "
 			"not underflow the 31-bit counter"
 		);
 
-		if (isNotifyNeeded(state + delta)) {
-			::std::lock_guard lock{m_};
+		auto const state = prev_state + delta;
+
+		if (isNotifyNeeded(state)) {
+			auto lock = ::std::lock_guard(m_);
 
 			counter_is_zero_.notify_all();
 		}
 	}
 
-	// waits until the counter drops to zero, and then acquire a data
-	void wait() noexcept
+	// blocks the current thread until the counter drops to zero, and
+	// (if requested) synchronizes with threads that
+	// performed synchronization when calling done
+	void wait(::std::memory_order order = ::std::memory_order_seq_cst) noexcept
 	{
-		auto state = state_.load(::std::memory_order_relaxed);
+		auto const state = getWaitingState(order);
 
-		// while helping bit isn't set
-		while (
-			(state & helping_bit) == 0 &&
-
-			!state_.compare_exchange_weak(
-				state,
-				state | helping_bit,
-				::std::memory_order_relaxed
-			)
-		);
-
-		if (getCounter(state) != 0) [[likely]] {
-			::std::lock_guard lock{m_};
-
-			while (isWaitNeeded(state | helping_bit)) {
-				counter_is_zero_.wait();
-			}
+		if (isCounterZero(state)) [[unlikely]] {
+			return;
 		}
 
-		// synchronize
-		UTILS_IGNORE(state_.load(::std::memory_order_acquire));
+		auto lock = ::std::lock_guard(m_);
+
+		while (isWaitNeeded(state, order)) {
+			counter_is_zero_.wait();
+		}
 	}
 
 private:
-	// State is transient if the counter has been lowered to zero
-	// and the helping_bit isn't set (bc of the carry bit)
+	// State is transient if the counter has dropped to zero and
+	// helping_bit isn't set (bc of the carry bit)
 	//
 	// In this case, progress is needed
-	static bool isTransientState(::std::uint64_t state) noexcept
+	static bool isTransientState(state_t const state) noexcept
 	{
 		return static_cast<::std::uint32_t>(state) == 0;
 	}
 
 	// progress has occurred if the version has not rolled back
-	bool tryToProgress(::std::uint64_t state) noexcept
+	bool tryToProgress(state_t state) noexcept
 	{
-		auto const version = static_cast<::std::uint32_t>(state >> 32) - 1;
+		auto const version = getVersion(state) - 1;
 
 		UTILS_IGNORE(state_.compare_exchange_strong(
 			state,
-			state | helping_bit,
+			setHelpingBit(state),
 			::std::memory_order_relaxed
 		));
 
-		return static_cast<::std::uint32_t>(state >> 32) != version;
+		return getVersion(state) != version;
 	}
 
-	bool isNotifyNeeded(::std::uint64_t state) noexcept
+	// true if the counter has dropped to zero (logically)
+	bool isNotifyNeeded(state_t const state) noexcept
 	{
 		return isTransientState(state) && tryToProgress(state);
 	}
 
-	// We need to keep waiting if for state_
-	// version corresponds, helping bit is set and counter is non-zero,
-	// or version is one more, helping bit isn't set, and counter is zero
-	// 
-	// It is checked in one comparison by bit magic/manipulation
-	bool isWaitNeeded(::std::uint64_t const waiting_state) const noexcept
+	// get the current state, and if necessary help the progress of the system
+	// (there is no progress if helping bit is not set)
+	state_t getWaitingState(::std::memory_order order) noexcept
 	{
-		constexpr auto counter_cleaning_mask = 0xFFFF'FFFF'8000'0000ull;
+		auto state = state_.load(order);
 
-		auto state = state_.load(::std::memory_order_relaxed);
+		while (true) {
+			if (testHelpingBit(state)) {
+				return state;
+			}
 
-		return ((state - 1) & counter_cleaning_mask) ==
-			(waiting_state & counter_cleaning_mask);
+			auto const new_state = setHelpingBit(state);
+
+			if (state_.compare_exchange_weak(state, new_state, order)) {
+				return new_state;
+			}
+
+			// backoff is not needed, bc it is not a spinlock, that is,
+			// after failed CAS, nothing prevents you from trying to
+			// set helping bit immediately again
+		}
 	}
 
-	static ::std::uint64_t getCounter(::std::uint64_t state) noexcept
+	// We need to keep waiting if for state_
+	// version corresponds, helping bit is set and counter is non-zero, or
+	// version is one more, helping bit isn't set, and counter is zero
+	// 
+	// It is checked in one comparison by bit magic/manipulation
+	bool isWaitNeeded(state_t const waiting_state,
+		::std::memory_order order) const noexcept
 	{
-		constexpr auto counter_mask = 0x7FFF'FFFFull;
+		auto const state = state_.load(order);
 
-		return state & counter_mask;
+		return dropCounter(state - 1) == dropCounter(waiting_state);
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	// true if the counter does not overflow (logically)
+	// after increasing by delta
+	static bool isAddOk(state_t const state, state_t const delta) noexcept
+	{
+		return delta - 1 < onlyCounter(state - 1);
+	}
+
+	// true if the counter does not underflow (logically)
+	// after reducing by delta
+	static bool isDoneOk(state_t const state, state_t const delta) noexcept
+	{
+		return delta - 1 < onlyCounter(0 - state);
+	}
+
+	static state_t onlyCounter(state_t const state) noexcept
+	{
+		return state & Mask::counter;
+	}
+
+	static state_t dropCounter(state_t const state) noexcept
+	{
+		return state & ~Mask::counter;
+	}
+
+	static bool isCounterZero(state_t const state) noexcept
+	{
+		return onlyCounter(state) == 0;
+	}
+
+	static state_t setHelpingBit(state_t const state) noexcept
+	{
+		return state | Mask::helping_bit;
+	}
+
+	static bool testHelpingBit(state_t const state) noexcept
+	{
+		return (state & Mask::helping_bit) != 0;
+	}
+
+	static ::std::uint32_t getVersion(state_t const state) noexcept
+	{
+		return static_cast<::std::uint32_t>(state >> 32);
 	}
 };
 
