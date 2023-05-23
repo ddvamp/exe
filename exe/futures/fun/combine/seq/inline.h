c898a5e7a4d795125996b1b0bfcfe2ced984534b
Artyom Kolpakov <ddvamp007@gmail.com>
2023-05-23 15:20:22 +0300

    Add InLine combinator

---
+++ exe/futures/fun/combine/seq/inline.h
@@ -0,0 +1,35 @@
+// Copyright (C) 2023 Artyom Kolpakov <ddvamp007@gmail.com>
+// Licensed under GNU GPL-3.0-or-later.
+// See file LICENSE or <https://www.gnu.org/licenses/> for details.
+
+#ifndef DDV_EXE_FUTURES_FUN_COMBINE_SEQ_INLINE_H_
+#define DDV_EXE_FUTURES_FUN_COMBINE_SEQ_INLINE_H_ 1
+
+#include <utility>
+
+#include "exe/executors/inline.h"
+#include "exe/futures/fun/mutator/mutator.h"
+#include "exe/futures/fun/syntax/pipe.h"
+
+namespace exe::futures {
+
+namespace pipe {
+
+struct [[nodiscard]] InLine : Mutator {
+	template <typename T>
+	Future<T> mutate(SemiFuture<T> f) noexcept
+	{
+		return setExecutor<T>(::std::move(f), executors::getInlineExecutor());
+	}
+};
+
+} // namespace pipe
+
+inline auto inLine() noexcept
+{
+	return pipe::InLine{};
+}
+
+} // namespace exe::futures
+
+#endif /* DDV_EXE_FUTURES_FUN_COMBINE_SEQ_INLINE_H_ */
