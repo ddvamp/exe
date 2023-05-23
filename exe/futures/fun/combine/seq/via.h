630ce8b72885254f46b5334626ae848abea2c648
Artyom Kolpakov <ddvamp007@gmail.com>
2023-05-23 10:14:27 +0300

    Add `via` combinator

---
+++ exe/futures/fun/combine/seq/via.h
@@ -0,0 +1,37 @@
+// Copyright (C) 2023 Artyom Kolpakov <ddvamp007@gmail.com>
+// Licensed under GNU GPL-3.0-or-later.
+// See file LICENSE or <https://www.gnu.org/licenses/> for details.
+
+#ifndef DDV_EXE_FUTURES_FUN_COMBINE_SEQ_VIA_H_
+#define DDV_EXE_FUTURES_FUN_COMBINE_SEQ_VIA_H_ 1
+
+#include <utility>
+
+#include "exe/executors/executor.h"
+#include "exe/futures/fun/mutator/mutator.h"
+#include "exe/futures/fun/syntax/pipe.h"
+
+namespace exe::futures {
+
+namespace pipe {
+
+struct [[nodiscard]] Via : Mutator {
+	executors::IExecutor &executor;
+
+	template <typename T>
+	Future<T> mutate(SemiFuture<T> f) noexcept
+	{
+		return setExecutor<T>(::std::move(f), executor);
+	}
+};
+
+} // namespace pipe
+
+inline auto via(executors::IExecutor &where) noexcept
+{
+	return pipe::Via{{}, where};
+}
+
+} // namespace exe::futures
+
+#endif /* DDV_EXE_FUTURES_FUN_COMBINE_SEQ_VIA_H_ */
