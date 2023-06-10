d815556218d4dcf2dc178da7dfb60d0640110008
Artyom Kolpakov <ddvamp007@gmail.com>
2023-06-10 18:27:04 +0300

    Rework `Strand`

---
+++ exe/executors/strand.cc
@@ -0,0 +1,226 @@
+// Copyright (C) 2023 Artyom Kolpakov <ddvamp007@gmail.com>
+// Licensed under GNU GPL-3.0-or-later.
+// See file LICENSE or <https://www.gnu.org/licenses/> for details.
+
+#include <atomic>
+
+#include "exe/executors/strand.h"
+
+#include "utils/abort.h"
+
+namespace exe::executors {
+
+class alignas(64) Strand::Impl : private TaskBase {
+private:
+	struct DummyTask : TaskBase {
+		DummyTask() noexcept
+		{
+			next_.store(this, ::std::memory_order_relaxed);
+		}
+
+		void run() noexcept override {}
+	};
+
+private:
+	IExecutor &where_;
+
+	::std::atomic_uint64_t owners_ = 1;
+
+	DummyTask dummy_;
+	TaskBase *head_ = &dummy_;
+	::std::atomic<TaskBase *> tail_ = &dummy_;
+
+public:
+	explicit Impl(IExecutor &where) noexcept
+		: where_(where)
+	{}
+
+	void release() noexcept
+	{
+		if (owners_.fetch_sub(1, ::std::memory_order_acq_rel) == 1) {
+			destroySelf();
+		}
+	}
+
+	void execute(TaskBase *task) noexcept;
+
+private:
+	void run() noexcept override;
+
+	bool enqueue(TaskBase *task) noexcept;
+
+	bool tryAcquire() noexcept;
+
+	bool tryPutDummy(TaskBase *expected) noexcept;
+
+	TaskBase *tryTakeNextTask(TaskBase *task) noexcept;
+
+	static TaskBase *loadNext(TaskBase *task) noexcept;
+
+	void afterAcquire(TaskBase *task) noexcept;
+
+	void share() noexcept
+	{
+		owners_.store(2, ::std::memory_order_relaxed);
+	}
+
+	void destroySelf() noexcept
+	{
+		delete this;
+	}
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+Strand::Strand(IExecutor &where)
+	: impl_(::new Impl(where))
+{}
+
+Strand::~Strand()
+{
+	impl_->release();
+}
+
+/* virtual */ void Strand::doExecute(TaskBase *task) noexcept
+{
+	impl_->execute(task);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void Strand::Impl::execute(TaskBase *task) noexcept
+{
+	if (enqueue(task)) {
+		// start a new critical section
+		// (relying on correct synchronization by executor)
+
+		// TODO: exception handling
+		try {
+			where_.execute(this);
+		} catch (...) {
+			UTILS_ABORT(
+				"exception while trying to start "
+				"a critical section of strand"
+			);
+		}
+	}
+}
+
+/* virtual */ void Strand::Impl::run() noexcept
+{
+	auto curr = head_;
+	auto next = curr->next_.load(::std::memory_order_relaxed);
+
+	if (next != &dummy_) [[unlikely]] {
+		goto run_task;
+	}
+
+dummy_next:
+	curr->run();
+
+	if ((curr = tryTakeNextTask(&dummy_))) {
+		dummy_.link(nullptr);
+		goto take_next;
+	}
+
+	release();
+	return;
+
+run_task:
+	::std::exchange(curr, next)->run();
+
+take_next:
+	if ((next = loadNext(curr))) {
+		goto run_task;
+	}
+
+	if (tryPutDummy(curr)) {
+		goto dummy_next;
+	}
+
+	if ((next = tryTakeNextTask(curr))) {
+		goto run_task;
+	}
+
+	release();
+}
+
+bool Strand::Impl::enqueue(TaskBase *task) noexcept
+{
+	if (tryAcquire()) {
+		afterAcquire(task);
+		return true;
+	}
+
+	task->link(nullptr);
+
+	auto prev =
+		tail_.exchange(task, ::std::memory_order_acq_rel)->
+		next_.exchange(task, ::std::memory_order_acq_rel);
+	
+	if (prev != &dummy_) [[likely]] {
+		return prev;
+	}
+
+	dummy_.link(nullptr);
+
+	if (tryPutDummy(task)) {
+		afterAcquire(task);
+		return true;
+	}
+
+	share();
+	return tryTakeNextTask(task);
+}
+
+bool Strand::Impl::tryAcquire() noexcept
+{
+	auto expected = static_cast<TaskBase *>(&dummy_);
+
+	return dummy_.next_.load(::std::memory_order_relaxed) == expected &&
+		dummy_.next_.compare_exchange_weak(
+			expected,
+			nullptr,
+			::std::memory_order_acquire,
+			::std::memory_order_relaxed
+		);
+}
+
+bool Strand::Impl::tryPutDummy(TaskBase *expected) noexcept
+{
+	return tail_.compare_exchange_strong(
+		expected,
+		&dummy_,
+		::std::memory_order_release,
+		::std::memory_order_relaxed
+	);
+}
+
+TaskBase *Strand::Impl::tryTakeNextTask(TaskBase *task) noexcept
+{
+	auto next = static_cast<TaskBase *>(nullptr);
+
+	task->next_.compare_exchange_strong(
+		next,
+		head_ = task,
+		::std::memory_order_release,
+		::std::memory_order_acquire
+	);
+
+	return next;
+}
+
+/* static */ TaskBase *Strand::Impl::loadNext(TaskBase *task) noexcept
+{
+	return task->next_.load(::std::memory_order_acquire);
+}
+
+void Strand::Impl::afterAcquire(TaskBase *task) noexcept
+{
+	share();
+
+	head_ = task;
+	task->link(&dummy_);
+}
+
+} // namespace exe::executors
--- exe/executors/strand.h
+++ exe/executors/strand.h
@@ -5,10 +5,40 @@
 #ifndef DDV_EXE_EXECUTORS_STRAND_H_
 #define DDV_EXE_EXECUTORS_STRAND_H_ 1
 
-#if defined(__x86_64__) || defined(_M_X64)
-#	include "exe/executors/arch/x86_64/strand.h"
-#else
-#	error "unsupported environment (not x86_64 architecture)"
-#endif
+#include "exe/executors/executor.h"
+
+namespace exe::executors {
+
+// Adapter for an executors that allows to
+// serialize asynchronous critical sections without using explicit locks
+// 
+// Instead of moving the "lock" between threads, it moves critical sections,
+// thereby allowing data to be in cache all the time
+//
+// Sending critical sections is wait-free except for
+// launching a new critical sections of the strand itself
+class Strand final : public IExecutor {
+private:
+	class Impl;
+
+	Impl *impl_;
+
+public:
+	~Strand();
+
+	Strand(Strand const &) = delete;
+	void operator= (Strand const &) = delete;
+
+	Strand(Strand &&) = delete;
+	void operator= (Strand &&) = delete;
+
+public:
+	explicit Strand(IExecutor &where);
+
+private:
+	void doExecute(TaskBase *critical_section) noexcept override;
+};
+
+} // namespace exe::executors
 
 #endif /* DDV_EXE_EXECUTORS_STRAND_H_ */
