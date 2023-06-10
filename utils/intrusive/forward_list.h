17029b2b1fd3402568064cdd6d60a51afc8c77db
Artyom Kolpakov <ddvamp007@gmail.com>
2023-06-10 18:13:21 +0300

    Add `link` method

--- utils/intrusive/forward_list.h
+++ utils/intrusive/forward_list.h
@@ -13,20 +13,29 @@ namespace detail {
 
 struct default_node_tag;
 
+template <typename T, typename>
+struct node_type {
+	using type = T;
+};
+
+template <typename T>
+struct node_type<default_node_tag, T> {
+	using type = T;
+};
+
 } // namespace detail
 
 template <typename T = detail::default_node_tag>
 struct intrusive_concurrent_forward_list_node {
-	using Node = T;
+	using Node =
+		detail::node_type<T, intrusive_concurrent_forward_list_node<T>>::type;
 
 	::std::atomic<Node *> next_ = nullptr;
-};
 
-template <>
-struct intrusive_concurrent_forward_list_node<detail::default_node_tag> {
-	using Node = intrusive_concurrent_forward_list_node;
-
-	::std::atomic<Node *> next_ = nullptr;
+	void link(Node *next) noexcept
+	{
+		next_.store(next, ::std::memory_order_relaxed);
+	}
 };
 
 } // namespace utils
