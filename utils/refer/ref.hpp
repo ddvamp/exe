1e28af8409a53ba941fc5d205531da4bf7ba0b23
Artyom Kolpakov <ddvamp007@gmail.com>
2024-05-21 08:20:07 +0300

    Add `utils/refer`

---
+++ utils/refer/ref.hpp
@@ -0,0 +1,143 @@
+// Copyright (C) 2023-2024 Artyom Kolpakov <ddvamp007@gmail.com>
+// 
+// Licensed under GNU GPL-3.0-or-later.
+// See file LICENSE or <https://www.gnu.org/licenses/> for details.
+
+#ifndef DDVAMP_UTILS_REFER_REF_HPP_INCLUDED_
+#define DDVAMP_UTILS_REFER_REF_HPP_INCLUDED_ 1
+
+#include <concepts>
+#include <cstddef>
+#include <functional>
+#include <type_traits>
+#include <utility>
+
+#include <utils/macro.hpp>
+
+namespace utils {
+
+template <typename Derived>
+class ref_count;
+
+
+template <typename T>
+inline constexpr bool is_ref_counted_v =
+    ::std::derived_from<T, ref_count<::std::remove_const_t<T>>> &&
+    requires (T const &t) { { t.destroy_self() } noexcept; };
+
+template <typename T>
+struct is_ref_counted : ::std::bool_constant<is_ref_counted_v<T>> {};
+
+template <typename T>
+concept ref_counted = is_ref_counted_v<T>;
+
+
+namespace detail {
+
+template <typename T>
+struct ref_validator {
+  constexpr ~ref_validator() {
+    static_assert(ref_counted<T>, "The class does not inherit ref_count or "
+                                  "has not defined the function destroy_self");
+  }
+};
+
+}  // namespace detail
+
+
+template <typename T>
+class [[nodiscard]] ref {
+ private:
+  T *ptr_ = nullptr;
+  UTILS_NO_UNIQUE_ADDRESS detail::ref_validator<T> v_;
+
+ public:
+  constexpr ~ref() {
+    dec_ref();
+  }
+
+  ref(ref const &that) noexcept : ptr_(that.ptr_) {
+    inc_ref();
+  }
+
+  ref(ref &&that) noexcept : ptr_(that.ptr_) {
+    that.ptr_ = nullptr;
+  }
+
+  ref &operator= (ref that) noexcept {
+    swap(that);
+    return *this;
+  }
+
+ public:
+  constexpr ref() = default;
+
+  constexpr ref(::std::nullptr_t) noexcept {}
+
+  constexpr explicit ref(T *ptr) noexcept : ptr_(ptr) {}
+
+  [[nodiscard]] ::std::size_t use_count() const noexcept {
+    return ptr_ ? ptr_->use_count() : 0;
+  }
+
+  void swap(ref &that) noexcept {
+    ::std::swap(ptr_, that.ptr_);
+  }
+
+  void reset() noexcept {
+    ref().swap(*this);
+  }
+
+  void reset(T *ptr) noexcept {
+    ref(ptr).swap(*this);
+  }
+
+  [[nodiscard]] T *get() const noexcept {
+    return ptr_;
+  }
+
+  [[nodiscard]] T &operator* () const noexcept {
+    return *get();
+  }
+
+  [[nodiscard]] T *operator-> () const noexcept {
+    return get();
+  }
+
+  explicit operator bool() const noexcept {
+    return ptr_;
+  }
+
+ private:
+  void inc_ref() const noexcept {
+    if (ptr_) {
+      ptr_->inc_ref();
+    }
+  }
+
+  void dec_ref() const noexcept {
+    if (ptr_) {
+      ptr_->dec_ref();
+    }
+  }
+};
+
+
+template <typename T>
+[[nodiscard]] bool operator== (ref<T> const &lhs, ref<T> const &rhs) noexcept {
+  return ::std::ranges::equal_to{}(lhs.get(), rhs.get());
+}
+
+template <typename T>
+[[nodiscard]] auto operator<=> (ref<T> const &lhs, ref<T> const &rhs) noexcept {
+  return ::std::compare_three_way{}(lhs.get(), rhs.get());
+}
+
+template <typename T>
+void swap(ref<T> &lhs, ref<T> &rhs) noexcept {
+  lhs.swap(rhs);
+}
+
+}  // namespace utils
+
+#endif /* DDVAMP_UTILS_REFER_REF_HPP_INCLUDED_ */
---
+++ utils/refer/ref_count.hpp
@@ -0,0 +1,50 @@
+// Copyright (C) 2023-2024 Artyom Kolpakov <ddvamp007@gmail.com>
+// 
+// Licensed under GNU GPL-3.0-or-later.
+// See file LICENSE or <https://www.gnu.org/licenses/> for details.
+
+#ifndef DDVAMP_UTILS_REFER_REF_COUNT_HPP_INCLUDED_
+#define DDVAMP_UTILS_REFER_REF_COUNT_HPP_INCLUDED_ 1
+
+#include <atomic>
+#include <cstddef>
+
+#include <utils/debug/assert.hpp>
+#include <utils/macro.hpp>
+
+#include "ref.hpp"
+
+namespace utils {
+
+template <typename Derived>
+class ref_count {
+ private:
+  mutable ::std::atomic<::std::size_t> cnt_;
+  UTILS_NO_UNIQUE_ADDRESS detail::ref_validator<Derived> v_;
+
+ public:
+  constexpr explicit ref_count(::std::size_t init = 1) noexcept : cnt_(init) {}
+
+  [[nodiscard]] ::std::size_t use_count() const noexcept {
+    return cnt_.load(::std::memory_order_relaxed);
+  }
+
+  void inc_ref() const noexcept {
+    UTILS_IGNORE(cnt_.fetch_add(1, ::std::memory_order_relaxed));
+  }
+
+  void dec_ref() const noexcept {
+    auto const before = cnt_.fetch_sub(1, ::std::memory_order_acq_rel);
+    if (before > 1) {
+      return;
+    }
+
+    UTILS_ASSERT(before != 0, "Accessing an ref_counted object "
+                              "with zero ref count");
+    static_cast<Derived const *>(this)->destroy_self();
+  }
+};
+
+}  // namespace utils
+
+#endif /* DDVAMP_UTILS_REFER_REF_COUNT_HPP_INCLUDED_ */
