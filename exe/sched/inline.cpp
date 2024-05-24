ef4f4998ee41f6c4ca9c1f95a09c2f2206024378
Artyom Kolpakov <ddvamp007@gmail.com>
2024-05-24 13:38:06 +0300

    Add `Inline` scheduler

---
+++ exe/sched/inline.cpp
@@ -0,0 +1,22 @@
+// Copyright (C) 2023-2024 Artyom Kolpakov <ddvamp007@gmail.com>
+// 
+// Licensed under GNU GPL-3.0-or-later.
+// See file LICENSE or <https://www.gnu.org/licenses/> for details.
+
+#include "inline.hpp"
+
+#include <utils/debug/assert.hpp>
+
+namespace exe::sched {
+
+/* virtual */ void Inline::Submit(task::TaskBase *task) noexcept {
+  UTILS_ASSERT(task, "nullptr instead of the task");
+  task->Run();
+}
+
+inline class Inline &Inline() noexcept {
+  static class Inline instance;
+  return instance;
+}
+
+}  // namespace exe::sched
