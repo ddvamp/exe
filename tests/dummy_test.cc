74c01071c6706e25e41463f88203696d98d4e0d8
Artyom Kolpakov <ddvamp007@gmail.com>
2025-09-08 15:07:18 +0300

    Add basic CMake support

---
+++ tests/dummy_test.cc
@@ -0,0 +1,7 @@
+// Copyright (C) 2025 Artyom Kolpakov <ddvamp007@gmail.com>
+// Licensed under GNU GPL-3.0-or-later.
+// See file LICENSE or <https://www.gnu.org/licenses/> for details.
+
+int main() {
+  return 0;
+}
