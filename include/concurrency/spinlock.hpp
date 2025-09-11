7f9e2c4e36112a3d322a8be92640724efada9aba
Artyom Kolpakov <ddvamp007@gmail.com>
2025-09-11 12:40:30 +0300

    Update `Spinlock`

--- include/concurrency/spinlock.hpp
+++ include/concurrency/spinlock.hpp
@@ -1,6 +1,6 @@
 //
-//
-//
+// spinlock.hpp
+// ~~~~~~~~~~~~
 //
 // Copyright (C) 2023-2025 Artyom Kolpakov <ddvamp007@gmail.com>
 //
@@ -11,34 +11,70 @@
 #ifndef DDVAMP_CONCURRENCY_SPINLOCK_HPP_INCLUDED_
 #define DDVAMP_CONCURRENCY_SPINLOCK_HPP_INCLUDED_ 1
 
-#include <atomic>
+#include <concurrency/pause.hpp>
 
-#include "pause.hpp"
+#include <util/debug/assert.hpp>
+
+#include <atomic>
+#include <new> // std::hardware_destructive_interference_size
 
 namespace concurrency {
 
-class Spinlock {
-private:
-	::std::atomic_flag locked_;
+class alignas (::std::hardware_destructive_interference_size) Spinlock {
+ private:
+  ::std::atomic_flag locked_;
 
-public:
-	[[nodiscard]] bool try_lock() noexcept
-	{
-		return !locked_.test(::std::memory_order_relaxed) &&
-			!locked_.test_and_set(::std::memory_order_acquire);
-	}
+ public:
+  ~Spinlock() {
+    UTIL_ASSERT(!IsLocked(), "Spinlock is destroyed when locked");
+  }
 
-	void lock() noexcept
-	{
-		while (!try_lock()) {
-			Pause();
-		}
-	}
+  Spinlock(Spinlock const &) = delete;
+  void operator= (Spinlock const &) = delete;
 
-	void unlock() noexcept
-	{
-		locked_.clear(::std::memory_order_release);
-	}
+  Spinlock(Spinlock &&) = delete;
+  void operator= (Spinlock &&) = delete;
+
+ public:
+  Spinlock() = default;
+
+  // This method is not intended to be used in a loop. Use Lock instead
+  [[nodiscard]] bool TryLock() noexcept {
+    return !locked_.test_and_set(::std::memory_order_acquire);
+  }
+
+  void Lock() noexcept {
+    while (!TryLock()) {
+      while (IsLocked()) {
+        Pause(); // [TODO]: Better spin wait
+      }
+    }
+  }
+
+  void Unlock() noexcept {
+    locked_.clear(::std::memory_order_release);
+  }
+
+  // Cpp17Lockable
+  // https://eel.is/c++draft/thread.req.lockable.req
+
+  // This method is not intended to be used in a loop. Use lock instead
+  [[nodiscard]] bool try_lock() noexcept {
+    return TryLock();
+  }
+
+  void lock() noexcept {
+    Lock();
+  }
+
+  void unlock() noexcept {
+    Unlock();
+  }
+
+ private:
+  [[nodiscard]] bool IsLocked() const noexcept {
+    return locked_.test(::std::memory_order_relaxed);
+  }
 };
 
 } // namespace concurrency
