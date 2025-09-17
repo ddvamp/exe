c747bd6f4c38179fa9d470a188ef538154b4dc5f
Artyom Kolpakov <ddvamp007@gmail.com>
2025-09-17 07:56:30 +0300

    Update `fiber::Event`

--- include/exe/fiber/sync/event.hpp
+++ include/exe/fiber/sync/event.hpp
@@ -1,6 +1,6 @@
 //
-//
-//
+// event.hpp
+// ~~~~~~~~~
 //
 // Copyright (C) 2023-2025 Artyom Kolpakov <ddvamp007@gmail.com>
 //
@@ -11,51 +11,136 @@
 #ifndef DDVAMP_EXE_FIBER_SYNC_EVENT_HPP_INCLUDED_
 #define DDVAMP_EXE_FIBER_SYNC_EVENT_HPP_INCLUDED_ 1
 
+#include <exe/fiber/api.hpp>
+#include <exe/fiber/core/awaiter.hpp>
+#include <exe/fiber/core/handle.hpp>
+
+#include <concurrency/intrusive/forward_list.hpp>
+#include <util/debug/assert.hpp>
+
+#include <atomic>
 #include <utility>
-
-#include "exe/fiber/sync/condvar.hpp"
-#include "exe/fiber/sync/mutex.hpp"
-
-#include "util/debug.hpp"
-#include "util/macro.hpp"
 
 namespace exe::fiber {
 
-// Synchronization primitive for waiting for a one-time notification
-// and sharing data with it
+/**
+ *  A synchronization primitive for waiting for a notification
+ *  and sharing data through it
+ */
 class Event {
-private:
-	bool notified_ = false;
-	Mutex m_; // protects the condvar
-	CondVar its_time_{m_};
+ private:
+  struct FiberNode : ::concurrency::IntrusiveForwardListNode<> {
+    FiberHandle handle;
+  };
 
-public:
-	void wait() noexcept
-	{
-		auto lock = ::std::lock_guard(m_);
+  struct Waiter final : IAwaiter, FiberNode {
+    Event &event;
 
-		while (!notified_) {
-			its_time_.wait();
-		}
-	}
+    explicit Waiter(Event &event) noexcept : event(event) {}
 
-	void notify() noexcept
-	{
-		auto lock = ::std::lock_guard(m_);
+    FiberHandle AwaitSymmetricSuspend(FiberHandle &&self) noexcept override {
+      handle = ::std::move(self);
+      event.WaitImpl(this);
+      return FiberHandle::Invalid();
+    }
+  };
 
-		UTIL_ASSERT(
-			!::std::exchange(notified_, true),
-			"one-time notification sent twice"
-		);
+  using Node = FiberNode::Node;
 
-		UTIL_IGNORE(its_time_.notify_all());
-	}
+ private:
+  Node dummy_;
+  ::std::atomic<Node *> tail_ = &dummy_;
 
-	// in case of instance reuse
-	void clear() noexcept
-	{
-		notified_ = false;
-	}
+  // To guarantee the expected implementation
+  static_assert(::std::atomic<Node *>::is_always_lock_free);
+
+ public:
+  ~Event() {
+    UTIL_ASSERT(tail_.load(::std::memory_order_relaxed) == &dummy_,
+                 "Event is destroyed during use");
+  }
+
+  Event(Event const &) = delete;
+  void operator= (Event const &) = delete;
+
+  Event(Event &&) = delete;
+  void operator= (Event &&) = delete;
+
+ public:
+  Event() = default;
+
+  // In case of an instance reuse
+  void Reset() noexcept {
+    dummy_.Link(nullptr);
+  }
+
+  void Wait() noexcept {
+    if (IsFired()) {
+      // Fast path
+      return;
+    }
+
+    Waiter awaiter(*this);
+    self::Suspend(awaiter);
+  }
+
+  void Fire() noexcept {
+    auto const waiters = GetWaiters();
+    if (!waiters) {
+      return;
+    }
+
+    Seal();
+    ScheduleWaiters(waiters);
+  }
+
+ private:
+  [[nodiscard]] bool IsFired() const noexcept {
+    return dummy_.next_.load(::std::memory_order_acquire) == &dummy_;
+  }
+
+  [[nodiscard]] Node *GetWaiters() noexcept {
+    return dummy_.next_.exchange(&dummy_, ::std::memory_order_release);
+  }
+
+  void WaitImpl(FiberNode *self) noexcept {
+    Node *expected = nullptr;
+    auto node = tail_.exchange(self, ::std::memory_order_acq_rel);
+    if (node->next_.compare_exchange_strong(expected, self,
+            ::std::memory_order_relaxed,
+            ::std::memory_order_acquire)) [[likely]] {
+      return;
+    }
+
+    if (node != &dummy_) [[likely]] {
+      node->Link(self);
+    } else {
+      node = self;
+      Seal();
+    }
+
+    ScheduleWaiters(node);
+  }
+
+  void Seal() noexcept {
+    tail_.exchange(&dummy_, ::std::memory_order_acquire)->Link(&dummy_);
+  }
+
+  void ScheduleWaiters(Node *waiter) const noexcept {
+    do {
+      auto next = waiter->Next();
+      if (!next) [[unlikely]] {
+        if (waiter->next_.compare_exchange_strong(next, waiter,
+                ::std::memory_order_release,
+                ::std::memory_order_relaxed)) [[likely]] {
+          return;
+        }
+      }
+
+      static_cast<Waiter &&>(*waiter).handle.Schedule();
+      waiter = next;
+    } while (waiter != &dummy_);
+  }
 };
 
 } // namespace exe::fiber
