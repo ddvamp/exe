929f42e24e059d92821e1bc8a03fb5a1a132a1df
Artyom Kolpakov <ddvamp007@gmail.com>
2026-01-08 16:09:11 +0300

    Simplify `ValueOf`

--- include/exe/future/fun/trait/value_of.hpp
+++ include/exe/future/fun/trait/value_of.hpp
@@ -11,27 +11,12 @@
 #ifndef DDVAMP_EXE_FUTURE_FUN_TRAIT_VALUE_OF_HPP_INCLUDED_
 #define DDVAMP_EXE_FUTURE_FUN_TRAIT_VALUE_OF_HPP_INCLUDED_ 1
 
-#include <exe/future/fun/type/future_fwd.hpp>
-
-#include <type_traits>
+#include <exe/future/fun/concept/future.hpp>
 
 namespace exe::future {
 
-namespace detail {
-
-template <typename>
-struct ValueOfImpl {};
-
-template <typename T>
-struct ValueOfImpl<SemiFuture<T>> : ::std::type_identity<T> {};
-
-template <typename T>
-struct ValueOfImpl<Future<T>> : ::std::type_identity<T> {};
-
-} // namespace detail
-
-template <typename T>
-using ValueOf = detail::ValueOfImpl<T>::type;
+template <concepts::Future F>
+using ValueOf = F::ValueType;
 
 } // namespace exe::future
 
