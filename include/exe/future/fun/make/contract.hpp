37c3d07f3c40f45bc8bf045eceb2cc90a5139821
Artyom Kolpakov <ddvamp007@gmail.com>
2026-01-10 03:46:02 +0300

    Separate `future::Promise`

--- include/exe/future/fun/make/contract.hpp
+++ include/exe/future/fun/make/contract.hpp
@@ -11,66 +11,12 @@
 #ifndef DDVAMP_EXE_FUTURE_FUN_MAKE_CONTRACT_HPP_INCLUDED_
 #define DDVAMP_EXE_FUTURE_FUN_MAKE_CONTRACT_HPP_INCLUDED_ 1
 
-#include <exe/future/fun/concept/future_value.hpp>
 #include <exe/future/fun/detail/shared_state.hpp>
 #include <exe/future/fun/make/contract_fwd.hpp>
-#include <exe/future/fun/result/error.hpp>
-#include <exe/future/fun/result/result.hpp>
 #include <exe/future/fun/type/future.hpp>
-
-#include <exception>
-#include <utility>
+#include <exe/future/fun/type/promise.hpp>
 
 namespace exe::future {
-
-class BrokenPromise : public ::std::exception {
- public:
-  [[nodiscard]] char const *what() const noexcept override {
-    return "exe::future::Promise was not used at destruction time";
-  }
-};
-
-template <concepts::FutureValue T>
-class Promise : protected detail::HoldState<T> {
-  friend Contract<T>;
-
- private:
-  using Base = detail::HoldState<T>;
-  using Base::Base;
-
- public:
-  ~Promise() {
-    if (IsValid()) [[unlikely]] {
-      auto const state = this->Release();
-      state->SetError(MakeError(BrokenPromise()));
-    }
-  }
-
-  Promise(Promise const &) = delete;
-  void operator= (Promise const &) = delete;
-
-  Promise(Promise &&) = default;
-  void operator= (Promise &&) = delete;
-
- public:
-  [[nodiscard("Pure")]] bool IsValid() const noexcept {
-    return this->HasState();
-  }
-
-  void SetResult(Result<T> &&r) && noexcept {
-    this->ReleaseChecked()->SetResult(::std::move(r));
-  }
-
-  void SetValue(T &&v) && noexcept {
-    this->ReleaseChecked()->SetValue(::std::move(v));
-  }
-
-  void SetError(Error &&e) && noexcept {
-    this->ReleaseChecked()->SetError(::std::move(e));
-  }
-};
-
-////////////////////////////////////////////////////////////////////////////////
 
 template <typename T>
 struct [[nodiscard]] Contract {
