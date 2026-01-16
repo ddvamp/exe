915fe3d586d63e434dbead80300776ff047a244c
Artyom Kolpakov <ddvamp007@gmail.com>
2026-01-16 12:00:04 +0300

    Update `All`

--- include/exe/future/fun/combine/concur/all.hpp
+++ include/exe/future/fun/combine/concur/all.hpp
@@ -14,11 +14,14 @@
 #include <exe/future/fun/concept/future.hpp>
 #include <exe/future/fun/core/contract.hpp>
 #include <exe/future/fun/core/operator.hpp>
+#include <exe/future/fun/result/error.hpp>
 #include <exe/future/fun/result/result.hpp>
 #include <exe/future/fun/trait/value_of.hpp>
 #include <exe/future/fun/type/future_fwd.hpp>
 #include <exe/future/fun/type/promise.hpp>
 #include <exe/runtime/inline.hpp>
+
+#include <util/mm/release_sequence.hpp>
 
 #include <atomic>
 #include <cstddef>
@@ -46,33 +49,41 @@ class AllImpl : private core::Operator {
     static_assert(::std::atomic_bool::is_always_lock_free);
     static_assert(::std::atomic_size_t::is_always_lock_free);
 
-    State(Promise<T> p, ::std::size_t sz) noexcept
+    explicit State(Promise<T> p) noexcept
         : p_(::std::move(p))
-        , live_(sz)
-        , ref_cnt_(sz) {}
+        , live_(sizeof...(Ts))
+        , ref_cnt_(sizeof...(Ts)) {}
 
     template <::std::size_t I>
-    void SetResult(Result<Ts...[I]> &&res) noexcept {
-      if (res.has_value()) {
-        ::std::get<I>(vals_).emplace(*::std::move(res));
+    void SetValue(Ts...[I] &&val) noexcept {
+      ::std::get<I>(vals_).emplace(::std::move(val));
 
-        if (live_.fetch_sub(1, ::std::memory_order_acq_rel) == 1) {
-          ::std::move(p_).SetValue(MakeTuple());
-        }
-      } else {
-        if (first_.exchange(false, ::std::memory_order_relaxed)) {
-          ::std::move(p_).SetError(::std::move(res).error());
-        }
+      if (live_.fetch_sub(1, ::std::memory_order_release) == 1) [[unlikely]] {
+        ::util::SyncWithReleaseSequences(live_);
+        ::std::move(p_).SetValue(MakeTuple());
       }
 
-      if (ref_cnt_.fetch_sub(1, ::std::memory_order_acq_rel) == 1) [[unlikely]] {
+      Release();
+    }
+
+    void SetError(Error &&err) noexcept {
+      if (first_.exchange(false, ::std::memory_order_relaxed)) [[unlikely]] {
+        ::std::move(p_).SetError(::std::move(err));
+      }
+
+      Release();
+    }
+
+    void Release() noexcept {
+      if (ref_cnt_.fetch_sub(1, ::std::memory_order_release) == 1) [[unlikely]] {
+        ::util::SyncWithReleaseSequences(ref_cnt_);
         delete this;
       }
     }
 
     [[nodiscard]] T MakeTuple() noexcept {
       return [&]<::std::size_t ...Idx>(::std::index_sequence<Idx...>) noexcept {
-          return ::std::tuple(*::std::get<Idx>(::std::move(vals_))...);
+        return ::std::tuple(*::std::get<Idx>(::std::move(vals_))...);
       }(::std::index_sequence_for<Ts...>{});
     }
   };
@@ -84,12 +95,16 @@ class AllImpl : private core::Operator {
 
     auto [f, p] = core::Contract<T>();
 
-    auto const state = ::new State(::std::move(p), sizeof...(Fs));
+    auto const state = ::new State(::std::move(p));
 
     [&]<::std::size_t ...Idx>(::std::index_sequence<Idx...>) {
       (..., SetCallback(SetScheduler(::std::move(fs), runtime::GetInline()),
                         [state](Result<trait::ValueOf<Fs>> &&res) noexcept {
-                          state->template SetResult<Idx>(::std::move(res));
+                          if (res.has_value()) {
+                            state->template SetValue<Idx>(*::std::move(res));
+                          } else {
+                            state->SetError(::std::move(res).error());
+                          }
                         }));
     }(::std::index_sequence_for<Fs...>{});
 
