fe5d4aa854d5512c7ef795963a848b7996325989
Artyom Kolpakov <ddvamp007@gmail.com>
2026-03-06 08:27:30 +0300

    Wip-06-03

--- include/exe/future1.5/all_thunks.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <exe/future2/thunk/make/ready.hpp> // IWYU pragma: export
-#include <exe/future2/thunk/seq/via.hpp> // IWYU pragma: export
-#include <exe/future2/thunk/seq/map.hpp> // IWYU pragma: export
